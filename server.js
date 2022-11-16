dd6421b638eaf1df33cb9438c59781ba947c4d69
BenS213 <57325318+BenS213@users.noreply.github.com>
2022-11-15 19:12:17 -0500

    initial heroku commit

--- server.js
+++ server.js
@@ -34,12 +34,8 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use(require("./controllers/"));
 
-// sequelize.sync({ force: false }).then(() => {
-//   app.listen(PORT, () => console.log("Now listening"));
-// });
-
 sequelize.sync({ force: false }).then(() => {
-  app.listen(process.env.PORT || 3001, '0.0.0.0', () => {
-      console.log("Server is running.");
-  });;
+  app.listen(PORT, () => console.log("Now listening"));
 });
+
+
