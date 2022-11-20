7c619af0aaecf5effa4ff73fdfa3756c342e9bf6
BenS213 <57325318+BenS213@users.noreply.github.com>
2022-11-20 14:48:16 -0500

    connected to heroku

--- server.js
+++ server.js
@@ -1,39 +1,42 @@
-const path = require("path");
-const express = require("express");
-const session = require("express-session");
-const exphbs = require("express-handlebars");
+const express = require('express');
+const sequelize = require('./config/connection');
+const path = require('path');
+const routes = require('./controllers');
+const exphbs = require('express-handlebars');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const helpers = require('./utils/helpers');
+
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sequelize = require("./config/connection");
-const SequelizeStore = require("connect-session-sequelize")(session.Store);
-
+//setup session
 const sess = {
-  secret: "Super secret secret",
+  secret: 'supersecretsessionsecrettext',
   cookie: { maxAge: 180000},
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
-    db: sequelize,
-  }),
+    db: sequelize
+  })
 };
 
-app.use(session(sess));
-
-const helpers = require("./utils/helpers");
-
+//handlebars initialization
 const hbs = exphbs.create({ helpers });
-
-app.engine("handlebars", hbs.engine);
-app.set("view engine", "handlebars");
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(require("./controllers/"));
+app.use(session(sess));
+
+//use routes
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log("Now listening"));
+  app.listen(PORT, () => console.log(`Now Listening on ${PORT}`));
 });
